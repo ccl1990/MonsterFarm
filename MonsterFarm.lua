6eea605d5780bc58d25f13b593a87bed49f61fd8
Michael <derccl@yahoo.de>
2020-09-19 01:32:33 +0200

    first commit

---
+++ MonsterFarm.lua
@@ -0,0 +1,12 @@
+-- Version 1.0
+
+-- Programm um verschiedene Monster über ein Redstone Signal spawnen zu lassen
+-- Menü
+
+-- Redstone enable
+local component = require("component")
+local rs = component.redstone
+lo.cal sides = require("sides")
+
+rs.setOutput(sides.back, 15)
+
